bf9d9efe328f8dce5a3734edb2b3e92cbd25cecd
Lê Hoàng Nam at home <namle392001@gmail.com>
2025-02-24 21:05:58 +0700

    'update'

--- Nextjs/my-porfolio/components/experient/Experient.tsx
+++ Nextjs/my-porfolio/components/experient/Experient.tsx
@@ -3,13 +3,27 @@ import React from 'react'
 const Experient = () => {
   return (
     <div className='container mx-auto p-3'>
-        <div className=''>
-            <div>
-                <h2>Em có 2 năm kinh nghiệm vị trí Frontend</h2>
+        <div className='flex justify-center'>
+            <div className='border-r-2 border-black p-3'>
+              <h2 className='text-6xl text-end leading-snug'>Em có <br></br> <strong>2 năm kinh nghiệm</strong> <br></br> vị trí Frontend</h2>
             </div>
-            <div>
-                <h2></h2>
+            <div className='max-w-[700px] w-full border-l-2 border-black p-3'>
+              <h2 className='text-4xl font-bold mb-5'>DFM Engineering</h2>
+              <ul>
+                <li>
+                  <p>Thâm niên : 2 năm ( 2023 - 2024 )</p>
+                </li>
+                <li>
+                  <p>Vị trí : Front-End Developer</p>
+                </li>
+                <li>
+                  <p>Mô tả công việc : Xây dựng template theo Figma, chuyển thiết kế thành mã nguồn HTML, CSS, JavaScript và tối ưu giao diện. Đảm bảo responsive, tương thích trình duyệt, tối ưu hiệu suất và cải thiện UX/UI.</p>
+                </li>
+              </ul>
             </div>
+        </div>
+        <div>
+          <h2></h2>
         </div>
     </div>
   )
