5a8e139dc96d19515ad6fc1827c2adedb612f010
Lê Hoàng Nam at home <namle392001@gmail.com>
2025-03-09 15:20:00 +0700

    'update'

--- Nextjs/clothing/app/layout.tsx
+++ Nextjs/clothing/app/layout.tsx
@@ -24,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
---
+++ Nextjs/clothing/app/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: 'Clothshop',
+  description: 'This is homepage'
+}
+
+export default function Home() {
+  return (
+   <>
+   
+   </>
+  );
+}
