6a1ab25b12a513abe44c4adf4e1645e8f76b7bd5
NoOne223 <namle392001@gmail.com>
2025-03-11 17:39:27 +0700

    update

--- Nextjs/cloth-shop/components/carousel/Carousel.tsx
+++ Nextjs/cloth-shop/components/carousel/Carousel.tsx
@@ -18,6 +18,7 @@ const Carousel = () => {
           delay: 3000,
           disableOnInteraction: false,
         }}
+        loop={true}
         speed={1000}
         navigation={true}
         modules={[Autoplay, Navigation]}
@@ -26,12 +27,6 @@ const Carousel = () => {
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </>
   )
