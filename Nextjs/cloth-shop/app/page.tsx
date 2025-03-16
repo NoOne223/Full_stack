982807012bcf1fbdd479caac8666767dad2e7e07
Lê Hoàng Nam at home <namle392001@gmail.com>
2025-03-16 20:30:44 +0700

    'update'

--- Nextjs/cloth-shop/app/page.tsx
+++ Nextjs/cloth-shop/app/page.tsx
@@ -1,7 +1,10 @@
 import Carousel from "@/components/carousel/Carousel";
 import Header from "@/components/header/Header";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: 'Shop clothes',
@@ -14,6 +17,27 @@ export default function Home() {
     <main>
       <section>
         <Carousel />
+      </section>
+      <section className="mt-16">
+        <div className="container mx-auto p-3">
+          <h2 className="title text-end max-w-[750px] ml-auto mb-9">The perfect addition to your closet</h2>
+          <div className="flex">
+            <div className="w-1/2 text-end">
+              <h3 className="sub-title">1. For men</h3>
+              <hr className="my-5"></hr>
+              <p className="text-2xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolores numquam magni minus qui explicabo repellendus, accusamus nesciunt voluptatum deserunt modi eum rerum fugiat quod eius enim! Doloribus, reiciendis adipisci.
+              </p>
+              <Link href=''>
+                Mua ngay
+                <FontAwesomeIcon className="ms-1.5" icon={faBagShopping} />
+              </Link>
+            </div>
+            <div className="w-1/2">
+              <Image className="w-full h-[700px] object-top object-cover rounded-2xl" src='/images/for-men.png' width={500} height={800} alt="content image"/>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
    </>
--- Nextjs/cloth-shop/components/header/LangChange.tsx
+++ Nextjs/cloth-shop/components/header/LangChange.tsx
@@ -3,44 +3,54 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 
 const languages = [
-  { code: 'vi', flag: '/images/vietnam.png' },
-  { code: 'en', flag: '/images/united-kingdom.png' },
-  { code: 'jp', flag: '/images/japan.png' }
+  {
+    "id": "1",
+    "lang": "vi",
+    "lang_name": "Tiếng Việt",
+    "lang_icon": "/images/vietnam.png"
+  },
+  {
+    "id": "2",
+    "lang": "en",
+    "lang_name": "English",
+    "lang_icon": "/images/united-kingdom.png"
+  },
+  {
+    "id": "3",
+    "lang": "ja",
+    "lang_name": "日本語",
+    "lang_icon": "/images/japan.png"
+  }
 ];
+
 const LangChange = () => {
   const [isDropdown, setIsDropdown] = useState(false);
-  const [selecteddLang, setSelectedLang] = useState(languages[0]);
+  const [selectedLang, setSelectedLang] = useState(languages[0]);
 
   const toggleDropdown = () => {
     setIsDropdown(!isDropdown);
-  }
+  };
 
   const selectLanguage = (lang: any) => {
     setSelectedLang(lang);
     setIsDropdown(false);
-  }
+  };
 
   return (
     <div className='relative'>
       <a className='cursor-pointer' onClick={toggleDropdown}>
-        <Image className='w-full h-10' src={selecteddLang.flag} width={500} height={500} alt='Language logo'/>
+        <Image className='w-full h-10' src={selectedLang.lang_icon} width={500} height={500} alt='Language logo'/>
       </a>
       <ul className={`absolute bg-primary rounded-2xl w-44 p-3 shadow-3xl shadow-secondary transition-all duration-500 ease-in-out ${isDropdown ? 'opacity-100 top-12 z-10' : 'opacity-0 -top-[120px] -z-10'}`}>
-        <li className='flex items-center gap-x-2 my-2 cursor-pointer group'>
-          <Image className='w-fit h-10' src='/images/vietnam.png' width={500} height={500} alt='Language logo'/>
-          <p className='group-hover:text-thirdary'>Tiếng Việt</p>
-        </li>
-        <li className='flex items-center gap-x-2 my-2 cursor-pointer group'>
-          <Image className='w-fit h-10' src='/images/united-kingdom.png' width={500} height={500} alt='Language logo'/>
-          <p className='group-hover:text-thirdary'>English</p>
-        </li>
-        <li className='flex items-center gap-x-2 my-2 cursor-pointer group'>
-          <Image className='w-fit h-10' src='/images/japan.png' width={500} height={500} alt='Language logo'/>
-          <p className='group-hover:text-thirdary'>日本語</p>
-        </li>
+        {languages.map((lang) => (
+          <li key={lang.id} className='flex items-center gap-x-2 my-2 cursor-pointer group' onClick={() => selectLanguage(lang)}>
+            <Image className='w-fit h-10' src={lang.lang_icon} width={500} height={500} alt='Language logo'/>
+            <p className='group-hover:text-thirdary'>{lang.lang_name}</p>
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default LangChange
+export default LangChange;
