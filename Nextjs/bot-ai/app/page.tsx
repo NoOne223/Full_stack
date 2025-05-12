73a74903f9b26b073ec7b3301b83a0334363a552
Lê Hoàng Nam at home <namle392001@gmail.com>
2025-05-12 12:40:43 +0700

    'update'

---
+++ Nextjs/bot-ai/app/page.tsx
@@ -0,0 +1,10 @@
+import BotModal from "@/components/botAI/BotModal";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-primary min-h-screen relative">
+     <BotModal />
+    </div>
+  );
+}
---
+++ Nextjs/bot-ai/components/botAI/BotModal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BotModal = () => {
+  return (
+    <div className='bg-white max-w-[750px] w-full max-h-[750px] h-full absolute inset-0 m-auto'>
+      
+    </div>
+  )
+}
+
+export default BotModal
